04408e117e8e2e1cd7b0f968980223e0cae96107
insano10 <jdommett@gmail.com>
2015-12-13 20:20:01 +0000

    fix up the location coordinates

--- src/main/java/com/insano10/Location.java
+++ src/main/java/com/insano10/Location.java
@@ -4,11 +4,11 @@ import twitter4j.GeoLocation;
 
 public enum Location
 {
-    NORTH_LONDON(new GeoLocation(51.574358, -0.100861), 4d),
-    SOUTH_LONDON(new GeoLocation(51.472250, -0.077556), 4d),
-    EAST_LONDON(new GeoLocation(51.518801, -0.009096), 4d),
-    WEST_LONDON(new GeoLocation(51.517895, -0.211563), 4d),
-    CENTRAL_LONDON(new GeoLocation(51.517592, -0.100861), 4d);
+    NORTH_LONDON(new GeoLocation(51.610289, -0.104315), 8d),
+    SOUTH_LONDON(new GeoLocation(51.382761, -0.121415), 8d),
+    EAST_LONDON(new GeoLocation(51.521564, 0.124194), 8d),
+    WEST_LONDON(new GeoLocation(51.502694, -0.359235), 8d),
+    CENTRAL_LONDON(new GeoLocation(51.503077, -0.126492), 8d);
 
     private final GeoLocation geoLocation;
     private final double radiusKm;
